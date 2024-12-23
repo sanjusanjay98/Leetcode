ac14a4d8ff644d2115af055fb907d926d71f9bd7
SANJAY R <145788680+sanjusanjay98@users.noreply.github.com>
2024-12-23 11:28:55 +0530

    [1526] [Time Beats: 99.90%] [Memory Beats: 72.28%] - LeetPush

--- 1526-Minimum-Number-of-Increments-on-Subarrays-to-Form-a-Target-Array.java
+++ 1526-Minimum-Number-of-Increments-on-Subarrays-to-Form-a-Target-Array.java
@@ -2,7 +2,7 @@ class Solution {
     public int minNumberOperations(int[] target) {
         int n = target.length;
         int ans = target[0];
-        for(int i = 1;i < n; i++) { 
+        for(int i = 1; i < n; i++) { 
             if (target[i] > target[i - 1]) {
                 ans += target[i] - target[i-1];
             }
