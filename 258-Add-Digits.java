b2d947db8850ccf972e022a5e660a2c93713782a
SANJAY R <145788680+sanjusanjay98@users.noreply.github.com>
2024-12-24 15:04:53 +0530

    [258] [Time Beats: 100.00%] [Memory Beats: 24.16%] - LeetPush

--- 258-Add-Digits.java
+++ 258-Add-Digits.java
@@ -1,15 +1,10 @@
 class Solution {
     public int addDigits(int num) {
-        if(num==0)  return 0;
-        while(num/10!=0){
-            int sum=0;
-            while(num/10!=0){
-                sum+=(num%10);
-                num/=10;
-            }
-            sum+=num;
-            num=sum;
-        }
-        return num;
+        if(num == 0)
+            return 0;
+        else if(num % 9 == 0)
+            return 9;
+        else 
+            return num % 9;
     }
 }
