d453a9df45254b433478550653d113018029be65
SANJAY R <145788680+sanjusanjay98@users.noreply.github.com>
2024-12-25 14:37:51 +0530

    [515] [Time Beats: 94.48%] [Memory Beats: 48.25%] - LeetPush

--- 515-Find-Largest-Value-in-Each-Tree-Row.java
+++ 515-Find-Largest-Value-in-Each-Tree-Row.java
@@ -13,45 +13,23 @@
  *     }
  * }
  */
-// class Solution {
-//     private List<Integer> result;
-//     public List<Integer> largestValues(TreeNode root) {
-//         result = new ArrayList<>();
-//         dfs(root, 0);
-//         return result;
-//     }
-
-//     public void dfs(TreeNode root, int level) {
-//         if(root != null) {
-//             int val = root.val;
-//             if(result.size() == level)
-//                 result.add(val);
-//             else
-//                 result.set(level, Math.max(result.get(level), val));
-//             dfs(root.left, level+1);
-//             dfs(root.right, level+1);
-//         }
-//     }
-// }
-
 class Solution {
-    List<Integer> ans;
+    private List<Integer> result;
     public List<Integer> largestValues(TreeNode root) {
-        ans = new ArrayList<>();
+        result = new ArrayList<>();
         dfs(root, 0);
-        return ans;
+        return result;
     }
 
-    private void dfs(TreeNode node, int l) {
-        if(node != null) {
-            int v = node.val;
-            if(l == ans.size()) {
-                ans.add(v);
-            } else {
-                ans.set(l, Math.max(ans.get(l), v));
-            }
-            dfs(node.left, l + 1);
-            dfs(node.right, l + 1);
+    public void dfs(TreeNode root, int level) {
+        if(root != null) {
+            int val = root.val;
+            if(result.size() == level)
+                result.add(val);
+            else
+                result.set(level, Math.max(result.get(level), val));
+            dfs(root.left, level+1);
+            dfs(root.right, level+1);
         }
     }
 }
