14e0169acbb72cc0645a9e1fee9907b7976c2d56
SANJAY R <145788680+sanjusanjay98@users.noreply.github.com>
2025-01-07 21:42:08 +0530

    [189] [Time Beats: 50.04%] [Memory Beats: 59.60%] - LeetPush

---
+++ 189-Rotate-Array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void reverse(int[] arr, int start, int end) {
+        while(start <= end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        if(n == 1)
+            return;
+        reverse(nums, 0, n - k - 1);
+        reverse(nums, n - k, n - 1);
+        reverse(nums, 0, n - 1);
+    }
+}
