74769f47dfec57cc168e88ddeb299811384f0eda
SANJAY R <145788680+sanjusanjay98@users.noreply.github.com>
2025-02-14 13:48:51 +0530

    [912] [Time Beats: 99.28%] [Memory Beats: 61.48%] - LeetPush

---
+++ 912-Sort-an-Array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        int[] counting = new int[2 * 50000 + 1];
+        for (int num : nums) {
+            counting[num + 50000]++;
+        }
+        int writeInd = 0;
+        for (int numberInd = 0; numberInd < counting.length; numberInd++) {
+            int freq = counting[numberInd];
+            while (freq != 0) {
+                nums[writeInd] = numberInd - 50000;
+                writeInd++;
+                freq--;
+            }
+        }
+        return nums;
+    }
+}
