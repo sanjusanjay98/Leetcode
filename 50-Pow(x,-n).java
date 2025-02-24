4a307133bb4af4414c5eab4e80ad29092f0851f1
SANJAY R <145788680+sanjusanjay98@users.noreply.github.com>
2025-02-24 20:45:12 +0530

    [50] [Time Beats: 100.00%] [Memory Beats: 87.26%] - LeetPush

--- 50-Pow(x,-n).java
+++ 50-Pow(x,-n).java
@@ -1,19 +1,24 @@
 class Solution {
     public double myPow(double x, int n) {
-        if(n < 0){
-            n = -n;
-            x = 1 / x;
-        } 
-        double pow = 1;
-        while(n != 0){
-            if((n & 1) != 0){
-                pow *= x;
-            } 
-                
-            x *= x;
-            n >>>= 1;
-            
-        } 
-        return pow;
+        double ans = 1;
+        long temp = n;
+        if (temp < 0) {
+            temp = temp * -1;
+        }
+        while (temp > 0) {
+            if (temp % 2 == 1) {
+                ans = ans * x;
+                temp = temp - 1;
+            } else {
+                x = x * x;
+                temp = temp / 2;
+            }
+        }
+
+        if (n < 0) {
+            ans = (double) 1.0 / (double) ans;
+        }
+
+        return ans;
     }
 }
